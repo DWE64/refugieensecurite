fd617cd28a874f888413cba87c846fcb664bbdd7
DWE64 <developpement.web.entreprise64@gmail.com>
2022-03-18 11:08:43 +0100

    ajout des mentions légales

---
+++ src/Controller/MentionLegaleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class MentionLegaleController extends AbstractController
+{
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+    #[Route('/mention_legale', name: 'app_mention_legale')]
+    public function index(): Response
+    {
+        return $this->render('mention_legale/index.html.twig', [
+            'controller_name' => $this->translator->trans('mentions.legales'),
+        ]);
+    }
+}
